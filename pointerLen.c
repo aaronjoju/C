f4ad84f239309b7358b5db2412b0c90a1d591d2a
aaronjoju <aaron.joju@mca.christuniversity.in>
2023-09-05 09:51:57 +0530

    stringPointerLen

---
+++ pointerLen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    char str[20]="efgwdfwefew", *ptr;
+    int count;
+    ptr = str;
+    count = 0;
+    while (*ptr != '\0')
+    {
+        count++;
+        ptr++;
+    }
+    printf("The length of the string is: %d", count);
+    return 0;
+}
